0cefec1fcd9e879044fe18744a5fb1f31ba97e5e
Bilmacken <lowe.granegard@elev.ga.ntig.se>
2025-03-25 14:08:20 +0100

    Slutförde slumpmässiga råden

--- static/Javascript/Slumpmässigt råd.js
+++ static/Javascript/Slumpmässigt råd.js
@@ -6,9 +6,9 @@ const OlikaRåd = [//Skapar en array med olika råd.
     'Att röra på sig regelbundet är en nyckel till att minska stress, genom regelbunden fysiskt aktivitet blir du dessutom starkare och orkar mer.',
     'En vuxen människa behöver sova ungefär 6 - 9 per dygn och när du sover bättre så klarar du lättare av stress och påfrestningar.',
     'Öva på att sätta gränser och prioritera dina egna behov. Att säga nej till för många åtaganden kan minska känslan av överväldigande stress',
-    'Råd 7',
-    'Råd 8',
-    'Råd 9',
+    'Om du känner att träningen är ett måste snarare än något du tycker om, kan det vara svårt att hålla motivationen uppe. Prova olika träningsformer för att hitta något som passar dig och som du ser fram emot att göra regelbundet.',
+    'Att använda rätt teknik i din träning är avgörande för att undvika skador och maximera dina resultat. Börja med lättare vikter och fokusera på att utföra rörelsen korrekt innan du ökar belastningen. Om du är osäker, be en tränare eller använd speglar för att kontrollera din form.',
+    'Försök att begränsa mängden godis, bakverk, glass och annat med mycket socker. Minska särskilt på söta drycker eftersom det är lätt att dricka stora mängder utan att känna sig mätt. Socker gör det lättare att bli överviktig på grund av den stora mängden kalorier.',
 ];
 
 for (let i = 0; i < RådElement.length; i++) { //Loopar igenom alla elementen och ger dom ett slumpmässigt råd.
